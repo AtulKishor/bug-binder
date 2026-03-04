1f1374abb5a0366a2430f63cb4dead20dff2a489
Atul Kishor <atulddpm@gmail.com>
2026-03-04 11:13:36 +0530

    fix migration issue

---
+++ bugbinder/_profile/migrations/0001_initial.py
@@ -0,0 +1,28 @@
+from django.conf import settings
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Profile',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(blank=True, max_length=100, null=True)),
+                ('mobile', models.CharField(blank=True, max_length=100, null=True)),
+                ('office', models.CharField(blank=True, max_length=100, null=True)),
+                ('github', models.CharField(blank=True, max_length=100, null=True)),
+                ('linkedin', models.CharField(blank=True, max_length=100, null=True)),
+                ('fixed', models.PositiveIntegerField(default=0)),
+                ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+    ]
